412e278933223adeeeecb96e4936a1a0b8353a39
Hima Janani Vinukollu <jananiswag1@gmail.com>
2025-05-06 13:50:28 +0530

    Loop.py

---
+++ Loop.py
@@ -0,0 +1,39 @@
+#PROGRAM CODE
+n = int(input())
+for i in range(n):
+    print(i**2)
+
+#QUESTION
+ask
+The provided code stub reads an integer, n, from STDIN. For all non-negative integers i<n , print i*i or i**2 .
+
+Example
+
+The list of non-negative integers that are less than n=3 is[0,1,2] . Print the square of each number on a separate line.
+
+0
+1
+4
+Input Format
+
+The first and only line contains the integer, .
+
+Constraints
+
+
+Output Format
+
+Print  lines, one corresponding to each .
+
+Sample Input 0
+
+5
+Sample Output 0
+
+0
+1
+4
+9
+16
+Language
+Python 3
